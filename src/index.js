b14bd2d3e224f142468b956177774707c70926d1
PrinceNiyonshuti <npprince47@gmail.com>
2022-02-21 16:38:36 +0200

    signup new user completed

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ import {
 	updateDoc,
 } from "firebase/firestore";
 
-import { getAuth } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 const firebaseConfig = {
 	apiKey: "AIzaSyBVxdliMe15NOjWKqc4tJ2i17hVyqaf6bc",
@@ -91,4 +91,21 @@ updateForm.addEventListener("submit", (e) => {
 	}).then(() => {
 		updateForm.reset();
 	});
+});
+
+// signup with email and password
+const signupForm = document.querySelector(".signup");
+signupForm.addEventListener("submit", (e) => {
+	e.preventDefault();
+	const email = signupForm.email.value;
+	const password = signupForm.password.value;
+
+	createUserWithEmailAndPassword(auth, email, password)
+		.then((cred) => {
+			console.log("user created ", cred.user);
+			signupForm.reset();
+		})
+		.catch((err) => {
+			console.log(err.message);
+		});
 });
