58ce801bb673210ada78ec862804d55d2971149d
PrinceNiyonshuti <npprince47@gmail.com>
2022-02-21 11:37:29 +0200

    setting up webpack done

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
 	mode: "development",
 	entry: "./src/index.js",
 	output: {
-		path: path.resolve(__dirname, "./"),
+		path: path.resolve(__dirname, "dist"),
 		filename: "bundle.js",
 	},
 	watch: true,
